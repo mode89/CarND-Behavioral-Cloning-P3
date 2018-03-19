3a93a58a47430684603d2a050a37ecd31921f33e
Mode <mode89@mail.ru>
2018-03-20 00:10:01 +0800

    normalize and crop input

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import csv
 import cv2
 import numpy
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 
 def load_log(path, images, steeringAngles):
     with open(path) as drivingLog:
@@ -23,7 +23,9 @@ def load_training_data():
 trainX, trainY = load_training_data()
 
 model = Sequential()
-model.add(Flatten(input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((60, 30), (0, 0))))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss="mse", optimizer="adam")
